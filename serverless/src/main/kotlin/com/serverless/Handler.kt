78550e1da9bb815fc31a2359e5f766218d529307
Ethan Perez <ethan.perez@onepeloton.com>
2019-11-26 21:30:28 -0600

    Add multiple subreddits

--- serverless/src/main/kotlin/com/serverless/Handler.kt
+++ serverless/src/main/kotlin/com/serverless/Handler.kt
@@ -4,6 +4,8 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.google.gson.Gson
 import com.serverless.models.Post
+import com.serverless.models.PostGroup
+import com.serverless.models.PostSource
 import com.serverless.models.TemplateData
 import com.serverless.services.RedditService
 import net.dean.jraw.models.SubredditSort
@@ -16,12 +18,16 @@ import software.amazon.awssdk.services.ses.model.SendTemplatedEmailRequest
 
 class Handler:RequestHandler<Map<String, Any>, ApiGatewayResponse> {
   override fun handleRequest(input:Map<String, Any>, context:Context):ApiGatewayResponse {
-
+    val subreddits = listOf("NBA", "NFL", "CFB", "Fitness", "Running")
     val redditClient = RedditService().client
-    val a = redditClient.subreddit("nba").posts().sorting(SubredditSort.TOP).timePeriod(TimePeriod.DAY).limit(5).build()
-    val posts = a.next().map { Post(it.title, it.url) }.toList()
 
-    val templateDataString = Gson().toJson(TemplateData("Here's an email", posts))
+    val postGroup = subreddits.map { subredditName ->
+      val rawPosts = redditClient.subreddit(subredditName).posts().sorting(SubredditSort.TOP).timePeriod(TimePeriod.DAY).limit(5).build()
+      val formattedPosts = rawPosts.next().map { Post(it.title, it.url, it.permalink, it.commentCount) }.toList()
+
+      PostGroup(PostSource.Reddit, "/r/$subredditName", formattedPosts)
+    }
+    val templateDataString = Gson().toJson(TemplateData("Update for Today", "", "", postGroup))
 
     val client = SesClient.builder().apply {
       region(Region.US_EAST_1)
--- serverless/src/main/kotlin/com/serverless/models/TemplateData.kt
+++ serverless/src/main/kotlin/com/serverless/models/TemplateData.kt
@@ -2,10 +2,24 @@ package com.serverless.models
 
 data class TemplateData(
         val subject: String,
-        val posts: List<Post>
+        val title: String,
+        val subheading: String,
+        val data: List<PostGroup>
 )
 
 data class Post(
         val title: String,
-        val href: String
+        val postHref: String,
+        val commentHref: String,
+        val commentCount: Int
 )
+
+data class PostGroup(
+        val source: PostSource,
+        val name: String,
+        val posts: List<Post>
+)
+
+enum class PostSource {
+    Reddit,
+}
